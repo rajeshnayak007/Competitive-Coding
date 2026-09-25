e5818c0d3d05fdd7037665b6be9e0596611512f0
rajeshnayak007 <rn3631138@gmail.com>
2026-09-25 14:56:56 +0530

    Add GFG solution: Largest in Array | Practice

--- GeeksForGeeks/Arrays/largest-in-array-practice.java
+++ GeeksForGeeks/Arrays/largest-in-array-practice.java
@@ -1,12 +1,12 @@
-// User function template for C
-int largest(int arr[], int n) {
-    // Code Here
-    int max=arr[0];
-    
-    for(int i=1;i<n; i++){
-        if(arr[i]>max){
-            max = arr[i];
-        }
+class Solution {
+    public static int largest(int[] arr) {
+        // code here
+                int max = arr[0];
+                for (int i = 1; i < arr.length; i++) {
+                    if (arr[i] > max) {
+                        max = arr[i];
+                    }
+                }
+                return max;
     }
-    return max;
 }
